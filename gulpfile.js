f6c2aba22e058c0283b6b83450d287c61dece7c8
Ahmad Awais <mrahmadawais@gmail.com>
2016-01-24 19:11:52 +0500

    Images task Done 💯

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,11 @@
  * A simple implementation of Gulp.
  *
  * Implements:
- * 			1. Sass to CSS conversion
- * 			2. JS concatenation
- * 			3. Watch files
- * 			4. Live reloads browser with BrowserSync
+ * 			1. Live reloads browser with BrowserSync
+ * 			2. CSS: Sass to CSS conversion, Autoprixing, Sourcemaps, CSS minification.
+ * 			3. JS: Concatenates & uglifies Vendor and Custom JS files.
+ * 			4. Images: Minifies PNG, JPEG, GIF and SVG images.
+ * 			5. Watches files for changes in CSS or JS
  *
  * @since 1.0.0
  * @author Ahmad Awais (@mrahmadawais)
@@ -18,6 +19,8 @@
   *
   * Project Configuration for gulp tasks.
   *
+  * In paths you can add <<glob or array of globs>>
+  *
   * Edit the variables as per your project requirements.
   */
 
@@ -25,26 +28,48 @@ var project             = 'WPGulpTheme'; // Project Name.
 var projecturl          = 'wpgulp.dev'; // Project URL. Could be something like localhost:8888.
 
 
-var styleSRC            = './assets/css/style.scss'; // Path to main .scss file
-var styleDestination    = './'; // Path to place the compiled CSS file
-// Defualt set to root folder
+var styleSRC            = './assets/css/style.scss'; // Path to main .scss file.
+var styleDestination    = './'; // Path to place the compiled CSS file.
+								// Defualt set to root folder.
 
 
-var jsVendorSRC         = './assets/js/vendors/*.js'; // Path to JS vendors folder
-var jsVendorDestination = './assets/js/'; // Path to place the compiled JS vendors file
-var jsVendorFile        = 'vendors'; // Compiled JS vendors file name
-// Default set to vendors i.e. vendors.js
+var jsVendorSRC         = './assets/js/vendors/*.js'; // Path to JS vendors folder.
+var jsVendorDestination = './assets/js/'; // Path to place the compiled JS vendors file.
+var jsVendorFile        = 'vendors'; // Compiled JS vendors file name.
+									// Default set to vendors i.e. vendors.js.
 
 
-var jsCustomSRC         = './assets/js/custom/*.js'; // Path to JS custom scripts folder
-var jsCustomDestination = './assets/js/'; // Path to place the compiled JS custom scripts file
-var jsCustomFile        = 'custom'; // Compiled JS custom file name
-// Default set to custom i.e. custom.js
+var jsCustomSRC         = './assets/js/custom/*.js'; // Path to JS custom scripts folder.
+var jsCustomDestination = './assets/js/'; // Path to place the compiled JS custom scripts file.
+var jsCustomFile        = 'custom'; // Compiled JS custom file name.
+									// Default set to custom i.e. custom.js.
+
+
+var imagesSRC			= './assets/img/raw/**/*.{png,jpg,gif,svg}'; // Source folder of images which should be optimized.
+var imagesDestination	= './assets/img/'; // Destination folder of optimized images. Must be different from the imagesSRC folder.
+
 
 // Watch files paths.
-var styleWatchFiles     = './assets/css/**/*.scss'; // Path to all *.scss files inside css folder and inside them
-var vendorJSWatchFiles  = './assets/js/vendors/*.js'; // Path to all vendors JS files
-var customJSWatchFiles  = './assets/js/custom/*.js'; // Path to all custom JS files
+var styleWatchFiles     = './assets/css/**/*.scss'; // Path to all *.scss files inside css folder and inside them.
+var vendorJSWatchFiles  = './assets/js/vendors/*.js'; // Path to all vendors JS files.
+var customJSWatchFiles  = './assets/js/custom/*.js'; // Path to all custom JS files.
+
+
+// Browsers you care about for autoprefixing.
+// Browserlist https://github.com/ai/browserslist
+const AUTOPREFIXER_BROWSERS = [
+    'last 2 version',
+    '> 1%',
+    'ie >= 9',
+    'ie_mob >= 10',
+    'ff >= 30',
+    'chrome >= 34',
+    'safari >= 7',
+    'opera >= 23',
+    'ios >= 7',
+    'android >= 4',
+    'bb >= 10'
+  ];
 
 
 /**
@@ -58,31 +83,20 @@ var gulp         = require('gulp'); // Gulp of-course
 var sass         = require('gulp-sass'); // Gulp pluign for Sass compilation
 var minifycss    = require('gulp-uglifycss'); // Minifies CSS files
 var autoprefixer = require('gulp-autoprefixer'); // Autoprefixing magic
-const AUTOPREFIXER_BROWSERS = [
-    'last 2 version',
-    '> 1%',
-    'ie >= 9',
-    'ie_mob >= 10',
-    'ff >= 30',
-    'chrome >= 34',
-    'safari >= 7',
-    'opera >= 23',
-    'ios >= 7',
-    'android >= 4',
-    'bb >= 10'
-  ]; // Browsers you care about for autoprefixing. Browserlist https://github.com/ai/browserslist
-
 
 // JS related plugins.
 var concat       = require('gulp-concat'); // Concatenates JS files
 var uglify       = require('gulp-uglify'); // Minifies JS files
 
+// Image realted plugins.
+var imagemin     = require('gulp-imagemin'); // Minify PNG, JPEG, GIF and SVG images with imagemin.
+
 // Utility related plugins.
-var rename      = require('gulp-rename'); // Renames files E.g. style.css -> style.min.css
-var sourcemaps  = require('gulp-sourcemaps'); // Maps code in a compressed file (E.g. style.css) back to it’s original position in a source file (E.g. structure.scss, which was later combined with other css files to generate style.css)
-var notify      = require('gulp-notify'); // Sends message notification to you
-var browserSync = require('browser-sync').create(); // Reloads browser and injects CSS. Time-saving synchronised browser testing.
-var reload      = browserSync.reload; // For manual browser reload.
+var rename       = require('gulp-rename'); // Renames files E.g. style.css -> style.min.css
+var sourcemaps   = require('gulp-sourcemaps'); // Maps code in a compressed file (E.g. style.css) back to it’s original position in a source file (E.g. structure.scss, which was later combined with other css files to generate style.css)
+var notify       = require('gulp-notify'); // Sends message notification to you
+var browserSync  = require('browser-sync').create(); // Reloads browser and injects CSS. Time-saving synchronised browser testing.
+var reload       = browserSync.reload; // For manual browser reload.
 
 
 /**
@@ -120,7 +134,7 @@ var reload      = browserSync.reload; // For manual browser reload.
 
 
 /**
- * Task: styles
+ * Task: `styles`.
  *
  * Compiles Sass, Autoprefixes it and Minifies CSS.
  *
@@ -163,7 +177,7 @@ gulp.task('styles', function () {
 
 
 /**
- * Task: vendorJS
+ * Task: `vendorJS`.
  *
  * Concatenate and uglify vendor JS scripts.
  *
@@ -188,7 +202,7 @@ gulp.task( 'vendorsJs', function() {
 
 
 /**
- * Task: customJS
+ * Task: `customJS`.
  *
  * Concatenate and uglify custom JS scripts.
  *
@@ -211,13 +225,40 @@ gulp.task( 'customJS', function() {
 		.pipe( notify( { message: 'TASK: "customJs" Completed!', onLast: true } ) );
 });
 
+
+/**
+ * Task: `images`.
+ *
+ * Minifies PNG, JPEG, GIF and SVG images.
+ *
+ * This task does the following:
+ * 		1. Gets the source of images raw folder
+ * 		2. Minifies PNG, JPEG, GIF and SVG images
+ * 		3. Generates and saves the optimized images
+ *
+ * This task will run only once, if you want to run it
+ * again, do it with the command `gulp images`.
+ */
+gulp.task( 'images', function() {
+	gulp.src( imagesSRC )
+		.pipe( imagemin( {
+					progressive: true,
+					optimizationLevel: 3, // 0-7 low-high
+					interlaced: true,
+					svgoPlugins: [{removeViewBox: false}]
+				} ) )
+		.pipe(gulp.dest( imagesDestination ))
+		.pipe( notify( { message: 'TASK: "images" Completed!', onLast: true } ) );
+});
+
+
+
  /**
   * Watch Tasks.
   *
   * Watches for file changes and runs specific tasks.
   */
-
- gulp.task( 'default', ['styles', 'vendorsJs', 'customJS', 'browser-sync'], function () {
+ gulp.task( 'default', ['styles', 'vendorsJs', 'customJS', 'images', 'browser-sync'], function () {
  	gulp.watch( styleWatchFiles, [ 'styles' ] );
  	gulp.watch( vendorJSWatchFiles, [ 'vendorsJs', reload ]  );
  	gulp.watch( customJSWatchFiles, [ 'customJS', reload ]  );
