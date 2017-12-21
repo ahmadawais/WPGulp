0f5d9eb18e8334c041ef367beddb0437629ac36c
Jeremy Englert <jeremy@cmsadgroup.com>
2017-12-20 21:20:33 -0700

    Setup default task to work with Gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -317,9 +317,9 @@ gulp.task( 'browser-sync', function() {
   *
   * Watches for file changes and runs specific tasks.
   */
- gulp.task( 'default', ['styles', 'vendorsJs', 'customJS', 'images', 'browser-sync'], function () {
+ gulp.task( 'default', gulp.parallel('styles', 'vendorsJs', 'customJS', 'images', 'browser-sync', function () {
   gulp.watch( projectPHPWatchFiles, reload ); // Reload on PHP file changes.
-  gulp.watch( styleWatchFiles, [ 'styles' ] ); // Reload on SCSS file changes.
-  gulp.watch( vendorJSWatchFiles, [ 'vendorsJs', reload ] ); // Reload on vendorsJs file changes.
-  gulp.watch( customJSWatchFiles, [ 'customJS', reload ] ); // Reload on customJS file changes.
- });
+  gulp.watch( styleWatchFiles, gulp.parallel( 'styles' ) ); // Reload on SCSS file changes.
+  gulp.watch( vendorJSWatchFiles, gulp.parallel( 'vendorsJs', reload ) ); // Reload on vendorsJs file changes.
+  gulp.watch( customJSWatchFiles, gulp.parallel( 'customJS', reload ) ); // Reload on customJS file changes.
+ }));
