7a16bead1d13ef2ad1f4ba6adec0f2afdf5837e7
Ram Ratan Maurya <mauryaratan@users.noreply.github.com>
2018-01-15 22:49:54 +0530

    Remove .dev usage

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
 	// START Editing Project Variables.
 	// Project options.
 	project: 'WPGulpTheme', // Project Name.
-	projectURL: 'wpgulp.dev', // Local project URL of your already running WordPress site. Could be something like local.dev or localhost:8888.
+	projectURL: 'wpgulp.local', // Local project URL of your already running WordPress site. Could be something like wpgulp.local or localhost:8888.
 	productURL: './', // Theme/Plugin URL. Leave it like it is, since our gulpfile.js lives in the root folder.
 	browserAutoOpen: false,
 	injectChanges: true,
