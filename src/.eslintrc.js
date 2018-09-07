ee416522ec51c8234a201edae18ddc798720ff6f
Ahmad Awais <me@AhmadAwais.com>
2018-09-07 17:02:55 +0500

    👌 IMPROVE: File Structure

---
+++ src/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  extends: ["eslint:recommended", "wordpress"],
+  parserOptions: {
+    sourceType: "module"
+  },
+  rules: {
+    indent: ["error", "tab"],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"],
+    semi: ["error", "always"]
+  }
+};
